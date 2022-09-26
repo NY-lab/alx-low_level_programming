e302750e863bf7ff9f5d9f3aea470f468a51fce9
Nyakno Efiom <nyaknoefiom@yahoo.com>
2022-09-26 13:35:36 -0700

    coomit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,77 +1,39 @@
 #include "main.h"
-
 #include <stddef.h>
 
-
-
 /**
+ * _strchr - locates char in string
+ * @s: pointer to string
+ * @c: char to locate
  *
- *  * _strchr - locates char in string
- *
- *   * @s: pointer to string
- *
- *    * @c: char to locate
- *
- *     *
- *
- *      * Return: pointer to first occurence of c in s
- *
- *       */
-
+ * Return: pointer to first occurence of c in s
+ */
 char *_strchr(char *s, char c)
-
 {
-
-		while (*s)
-
-				{
-
-							if (*s == c)
-
-											return (s);
-
-									s++;
-
-										}
-
-			return (NULL);
-
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
 }
 
-
-
 /**
+ * _strspn - gets length of prefix substring
+ * @s: string to check
+ * @accept: bytes composing prefix
  *
- *  * _strspn - gets length of prefix substring
- *
- *   * @s: string to check
- *
- *    * @accept: bytes composing prefix
- *
- *     *
- *
- *      * Return: integer length of substring
- *
- *       */
-
+ * Return: integer length of substring
+ */
 unsigned int _strspn(char *s, char *accept)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (*s && _strchr(accept, *s))
-
-					{
-
-								s++;
-
-										i++;
-
-											}
-
-				return (i);
-
+	while (*s && _strchr(accept, *s))
+	{
+		s++;
+		i++;
+	}
+	return (i);
 }
